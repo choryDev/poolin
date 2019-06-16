a149e9cd1324a1a7a1f69a5562df24c7dfd70af9
Choi <ijunc2@Chois-MacBook-Pro.local>
2019-06-16 14:14:50 +0900

    updated

--- src/main/java/in/pool/server/service/workspace/WSGetService.java
+++ src/main/java/in/pool/server/service/workspace/WSGetService.java
@@ -1,4 +1,19 @@
 package in.pool.server.service.workspace;
 
-public class WSGetService {
+import in.pool.server.dto.RequestDto;
+import in.pool.server.dto.ResponseDto;
+import in.pool.server.dto.ResponseListDto;
+import in.pool.server.service.CoreSpace;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WSGetService extends CoreSpace {
+
+    public ResponseDto getListWorkspace(RequestDto requestDto){
+        ResponseListDto response = new ResponseListDto();
+        response.setList((List)getList("ws.workspace.getListOfWorkspace", requestDto));
+        return response;
+    }
 }
